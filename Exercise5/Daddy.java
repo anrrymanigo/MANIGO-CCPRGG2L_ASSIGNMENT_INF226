11113faf04e6ed324e1b510d4e43d21bb9e7305d
Anrry James Manigo - INF226 <119920820+anrrymanigo@users.noreply.github.com>
2023-02-10 14:22:56 +0800

    Add files via upload

---
+++ Exercise5/Daddy.java
@@ -0,0 +1,9 @@
+public class Daddy {
+
+    protected String surname = "Manigo";
+
+    protected void showSurname(){
+        System.out.println("my family name is " + surname);
+    }
+    
+}
---
+++ Exercise5/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    private String name = "james";
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String personName){
+        this.name = personName;
+    }
+}
